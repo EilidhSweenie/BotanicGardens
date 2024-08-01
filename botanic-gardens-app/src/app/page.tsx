e56d7e97a1888a6e177d5bdb79904967334d2b20
EilidhSweenie <EilidhSweenie@hotmail.com>
2024-08-01 23:40:18 +0100

    Set up basic app

---
+++ botanic-gardens-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+// src/app/about/page.js
+import React from 'react';
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="home page" content="Home Page" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
